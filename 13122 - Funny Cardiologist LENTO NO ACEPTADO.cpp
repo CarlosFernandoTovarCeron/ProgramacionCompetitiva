9b011a91671871961f9f4c67e8d75662e453ca1e
CarlosFernandoTovarCeron <carlos.fernando.tovar@correounivalle.edu.co>
2017-10-17 07:10:09 -0500

    Runtime error con menos dimensiones

--- 13122 - Funny Cardiologist LENTO NO ACEPTADO.cpp
+++ 13122 - Funny Cardiologist LENTO NO ACEPTADO.cpp
@@ -4,15 +4,17 @@
 #include <cstring>
 #include <stdio.h>
 #include <time.h>
+#include <utility>
+#include <vector>
 
 using namespace std;
 
 double x[258];
 double y[258];
 
-double dp[258][258][258];
+double dp[300][300];
 
-#define INF 100000000.0
+#define INF 1000000000.0
 
 double distanceCalculate(double x1, double y1, double x2, double y2){
 	double x = x1 - x2;
@@ -23,54 +25,72 @@ double distanceCalculate(double x1, double y1, double x2, double y2){
 	return dist;
 }
 
-void reiniciar(){
-	for(int i=0; i<258; i++){
-		std::fill(dp[i][0], dp[i][0] + 258 * 258, -1.0);
+struct segmento{
+	int posf;
+	double x1;
+	double y1;
+	double x2;
+	double y2;
+};
+
+int n;
+vector < vector <segmento> > segmentos;
+
+double c(int j, int faltan){
+	if(j==n-1 and faltan==0)
+		return 0.0;
+		
+	if(j==n-1 and faltan!=0){
+		return INF;
 	}
-}
-
-double c(int a, int b, int m){
-    if(a==0 and m!=0)
-        return INF;
-
-    if(m==0)
-        return distanceCalculate(x[0], y[0], x[b], y[b]);
-
-    if(dp[a][b][m]!=-1.0){
-    	return dp[a][b][m];
-    }
-
-    double iz = c(a-1, a, m-1) + distanceCalculate(x[a], y[a], x[b], y[b]);
-    double der = c(a-1, b, m);
-
-    dp[a][b][m] =  min(iz, der);
-
-    return min(iz, der);
-
+	
+	if(dp[j][faltan] != -1.0) return dp[j][faltan];
+	
+	double minimo = INF;
+	for(int k=0; k<segmentos[j].size(); k++){
+		minimo = min(distanceCalculate(segmentos[j][k].x1, segmentos[j][k].y1, segmentos[j][k].x2, segmentos[j][k].y2)
+			 +c(segmentos[j][k].posf, faltan-1), minimo);
+	}
+	
+	dp[j][faltan] = minimo;
+	
+	return minimo;
 }
 
 int main() {
-    clock_t tStart = clock();
-	freopen("in.txt", "r", stdin);
-	freopen("out.txt", "w", stdout);
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-    cout << fixed;
-    cout << setprecision(3);
-    int n, k;
+	
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);
+cout << fixed;
+cout << setprecision(3);
+    
+    int k;
     while(cin >> n >> k){
-    	reiniciar();
-        for(int i=0; i<n; i++){
+	std::fill(dp[0] + 0, dp[257] + 257, -1.0);
+
+for(int i=0; i<n; i++){
             double aux;
             cin >> aux;
             x[i] = aux;
             cin >> aux;
             y[i] = aux;
         }
-        cout << c(n-2,n-1, n-k-2) << endl;
+        
+        
+        for(int i=0; i<n-1; i++){
+        	vector <segmento> col;
+        	for(int j=i+1; j<n; j++){
+        		segmento aux; aux.posf = j;
+        		aux.x1 = x[i]; aux.x2 = x[j];
+        		aux.y1 = y[i]; aux.y2 = y[j];
+        		col.push_back(aux);
+        	}
+        	segmentos.push_back(col);
+        }
+        
+        cout << c(0, n-k-1) << endl;
+        
     }
-
-    printf("Time taken: %.2fs\n", (double)(clock() - tStart)/CLOCKS_PER_SEC);
 
 	return 0;
 }

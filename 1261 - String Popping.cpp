51bc92057e4675bf2099f0cf17d865717ca4d679
CarlosFernandoTovarCeron <carlos.fernando.tovar@correounivalle.edu.co>
2018-03-27 12:22:25 -0500

    Aceptado sin bitmask, con map

--- 1261 - String Popping.cpp
+++ 1261 - String Popping.cpp
@@ -1,142 +1,65 @@
 #include <iostream>
+#include <map>
 #include <string>
-#include <cstring>
 
 using namespace std;
 
-//1 1 3 3 2
+map <string, int> m;
+map <string, int>::iterator it;
 
-bool letra[25];
-int pesos[25];
-int aux, k, l;
-int dp[1 << 25];
-
-int c(int b){
-    bool prendidos = true;
-    for(int i=0; i<k; i++){
-        int mask=1<<i;
-        if(!(b&mask)){
-            prendidos = false;
-            break;
-        }
-    }
+bool c(string & s){
+    if(s=="") return true;
     
-    if(prendidos){
-        return true;
-    }
-    
-    if(dp[b]!=-1){
-        return dp[b];
+    it = m.find(s);
+    if(it!=m.end()){
+        return m[s];
     }
     
     bool f = false;
-    for(int i=0; i<k; i++){
-        int mask = 1 << i;
-        if(!(b&mask)){
-            int iz = -1, der = -1;
-            for(int j=i+1; j<k; j++){
-                int mask2 = 1 << j;
-                if(!(b&mask2)){
-                    der = j;
-                    bool r = b&mask;
-                    break;
+    char referencia = s[0];
+    int i=0, j=0;
+    for(int k=0; k<s.length(); k++){
+        if(s[k]!=referencia){
+            if(j-i+1>=2){
+                string cs = s.substr(0, i);
+                if(j+1<s.length()){
+                    cs+= s.substr(j+1, s.length()-(j+1));
                 }
+                bool nodo = c(cs);
+                m[cs] = nodo;
+                f = f or nodo;
             }
-            for(int j=i-1; j>=0; j--){
-                int mask2 = 1 << j;
-                if(!(b&mask2)){
-                    iz = j;
-                    break;
-                }
-            }
-            
-            
-            int pesoaux = pesos[i];
-
-            if(iz!=-1){
-                if(letra[iz]==letra[i]){
-                    pesoaux+=pesos[iz];            
-                }
-            }
-            
-            if(der!=-1){
-                if(letra[der]==letra[i]){
-                    pesoaux+=pesos[der];
-                }
-            }
-            
-            //cout << "i " << i  << " iz " << iz << " der " << der << " peso " << pesoaux <<  endl;  
-
-            if(pesoaux>=2){
-                int cb = b;
-                int mask = 1 << iz;
-                if(iz!=-1){
-                    if(letra[iz]==letra[i])
-                        cb = cb|mask;
-                }
-                mask = 1 << der;
-                if(der!=-1){
-                    if(letra[der]==letra[i]){
-                        cb = cb|mask;
-                    }
-                }
-                mask = 1 << i;
-                cb = cb|mask;
-                dp[cb] = c(cb); 
-                f = f or dp[cb];
-            }
-
+            //cout << i << " " << j << endl;
+            i = k;
+            referencia = s[k];
         }
+        j = k;
+    }
+    //cout << i << " " << j << endl;
+    if(j-i+1>=2){
+        string cs = s.substr(0, i);
+        if(j+1<s.length()){
+            cs+= s.substr(j+1, s.length()-(j+1));
+        }
+        bool nodo = c(cs);
+        m[cs] = nodo;
+        f = f or nodo;
     }
     
-    dp[b] = f;
+    m[s] = f;
     
     return f;
-    
 }
-
-//   aaa1aaa1aaaa
 
 int main() {
     int casos;
     cin >> casos;
     while(casos--){
-        memset(dp, -1, sizeof(dp));
         string linea;
         cin >> linea;
-        if(linea[linea.length()-1]=='a'){
-            linea+="b";
-        }else{
-            linea+="a";
-        }
-        aux = 0; k = 0; l = linea[0] == 'a';
-        for(int i=0; i<linea.length(); i++){
-            if(l){
-                if(linea[i]!='a'){
-                    pesos[k] = aux;
-                    letra[k] = l;
-                    aux = 0;
-                    l = linea[i] == 'a';
-                    k++;
-
-                }
-                aux++;
-            }else{
-                if(linea[i]=='a'){
-                    pesos[k] = aux;
-                    letra[k] = l;
-                    aux = 0;
-                    l = linea[i] == 'a';
-                    k++;
-                }
-                aux++;
-            }
-            
-        }
-        
-        cout << c(0) << endl;
-        
+        cout << c(linea) << endl;
 
     }
+    
 	return 0;
 }
